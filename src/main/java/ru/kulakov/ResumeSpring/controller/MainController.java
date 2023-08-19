e8faef34726a381d8315484de09b39ce5cca3a90
LeonidKulakov <107943543+LeonidKulakov@users.noreply.github.com>
2023-08-19 18:10:32 +0300

    HTML_commit

---
+++ src/main/java/ru/kulakov/ResumeSpring/controller/MainController.java
@@ -0,0 +1,14 @@
+package ru.kulakov.ResumeSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+@GetMapping
+    public String getMainPage(Model model){
+        model.addAttribute("title","Главная страница");
+        return "home";
+    }
+}
